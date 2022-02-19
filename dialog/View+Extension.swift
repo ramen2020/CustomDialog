c6598de87b2b02bd72a7bdf3c0bf136f8877b1c1
ramen2020 <vfvsfv.dlk22@gmail.com>
2022-02-20 00:45:02 +0900

    fix full screen modifier

--- dialog/View+Extension.swift
+++ dialog/View+Extension.swift
@@ -18,11 +18,6 @@ extension View {
         self
             .onChange(of: isPresented.wrappedValue) { newValue in
                 if newValue {                    
-                    let scenes = UIApplication.shared.connectedScenes
-                    let windowScene = scenes.first as? UIWindowScene
-                    let window = windowScene?.windows.last
-                    
-                    window?.isHidden = true
                     let view = content()
                     let viewController = UIHostingController(rootView: view)
                     viewController.view.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.42)

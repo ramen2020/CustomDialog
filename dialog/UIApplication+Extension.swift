340dd3cb09f1bbff5d61d3963cf69ffd1d39ee8a
ramen2020 <vfvsfv.dlk22@gmail.com>
2022-02-19 23:51:27 +0900

    add method to get Top ViewContoroller

--- dialog/UIApplication+Extension.swift
+++ dialog/UIApplication+Extension.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+import SwiftUI
+
+extension UIApplication {
+    public func getTopViewController() -> UIViewController? {
+        guard let window = UIApplication.shared
+                .connectedScenes
+                .filter({$0.activationState == .foregroundActive})
+                .map({$0 as? UIWindowScene})
+                .compactMap({$0})
+                .first?.windows
+                .filter({$0.isKeyWindow}).first else {
+            return nil
+        }
+
+        window.makeKeyAndVisible()
+
+        guard let rootViewController = window.rootViewController else {
+            return nil
+        }
+
+        var topController = rootViewController
+        while let newTopController = topController.presentedViewController {
+            topController = newTopController
+        }
+
+        return topController
+  }
+
+    public func dismissModalView() {
+        UIApplication.shared.getTopViewController()?.dismiss(animated: true, completion: nil)
+    }
+}

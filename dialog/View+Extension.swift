45f230cc5a56b99599c9cd55dbf17350a1c2ac89
ramen2020 <vfvsfv.dlk22@gmail.com>
2022-02-20 00:01:41 +0900

    add full screen modifier

--- dialog/View+Extension.swift
+++ dialog/View+Extension.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+import SwiftUI
+
+//MARK: フルスクリーン
+extension View {
+    // 透過されて表示するフルスクリーン
+    public func fullOverFullScreenView<Content>(
+        isPresented: Binding<Bool>,
+        @ViewBuilder content: @escaping () -> Content
+    ) -> some View where Content: View {
+        self
+            .onChange(of: isPresented.wrappedValue) { newValue in
+                if newValue {                    
+                    let scenes = UIApplication.shared.connectedScenes
+                    let windowScene = scenes.first as? UIWindowScene
+                    let window = windowScene?.windows.last
+                    
+                    window?.isHidden = true
+                    let view = content()
+                    let viewController = UIHostingController(rootView: view)
+                    viewController.view.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.42)
+                    viewController.modalPresentationStyle = .overFullScreen
+
+                    DispatchQueue.main.async {
+                        guard let tvc = UIApplication.shared.getTopViewController() else {
+                            return
+                        }
+                        tvc.modalPresentationStyle = .overCurrentContext
+                        tvc.present(viewController, animated: true, completion: nil)
+                    }
+                } else {
+                    UIApplication.shared.dismissModalView()
+                }
+            }
+    }
+}

bbd58f4d8696495e1f928139d5c0d91c72f1f89c
ramen2020 <vfvsfv.dlk22@gmail.com>
2022-02-23 15:43:21 +0900

    fix ContentView

--- dialog/ContentView.swift
+++ dialog/ContentView.swift
@@ -9,6 +9,49 @@ import SwiftUI
 
 struct ContentView: View {
     
+    var body: some View {
+        NavigationView {
+            TabView {
+                FirstPage()
+                    .tabItem {
+                        Image(systemName: "1.circle.fill")
+                    }
+                SecondPage()
+                    .tabItem {
+                        Image(systemName: "2.circle.fill")
+                    }
+            }
+            .navigationBarTitle("Dialog / PopUp")
+        }
+    }
+}
+
+struct FirstPage: View {
+    
+    @State var isPresented: Bool = false
+    
+    var body: some View {
+        VStack {
+            Button (action: {
+                isPresented = true
+            }) {
+                Text("open")
+                    .foregroundColor(Color.white)
+                    .font(.system(size: 18, weight: .semibold))
+                    .frame(width: 250, height: 50)
+                    .background(Color.blue)
+                    .cornerRadius(10)
+            }
+        }
+        .background(Color.white)
+        .fullOverFullScreenView(isPresented: $isPresented){
+            ModalView(isPresented: $isPresented)
+        }
+    }
+}
+
+struct SecondPage: View {
+    
     @State var isPresented: Bool = false
     
     var body: some View {

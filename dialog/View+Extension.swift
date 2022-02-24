b43eb08d445dd6f4f91443b4a0c6e7ea8a5da4d9
ramen2020 <vfvsfv.dlk22@gmail.com>
2022-02-25 00:01:05 +0900

    add half modal

--- dialog/View+Extension.swift
+++ dialog/View+Extension.swift
@@ -35,4 +35,38 @@ extension View {
                 }
             }
     }
+    
+    public func halfModalView<Content>(
+        isPresented: Binding<Bool>,
+        @ViewBuilder content: @escaping () -> Content
+    ) -> some View where Content: View {
+        self
+            .onChange(of: isPresented.wrappedValue) { newValue in
+                if newValue {
+                    let view = content()
+                    
+                    let view2 = VStack {}
+                        .frame(maxWidth: .infinity,maxHeight: .infinity)
+                        .overlay(
+                            view, alignment: .bottom
+                        )
+                        .ignoresSafeArea(edges: .bottom)
+                    
+                    let viewController = UIHostingController(rootView: view2)
+                    viewController.view.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.42)
+                    viewController.modalPresentationStyle = .overFullScreen
+                    
+                    DispatchQueue.main.async {
+                        guard let tvc = UIApplication.shared.getTopViewController() else {
+                            return
+                        }
+                        tvc.modalPresentationStyle = .overCurrentContext
+                        tvc.present(viewController, animated: true, completion: nil)
+                        
+                    }
+                } else {
+                    UIApplication.shared.dismissModalView()
+                }
+            }
+    }
 }
